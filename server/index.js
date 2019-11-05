81326bdf5321e2bcff0e1ede1d497aa6d624f479
Anna Collins <annacollins85@gmail.com>
2019-11-05 19:18:13 +0100

    fix: remove hardcoded port

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const { contentfulService } = require('./contentful-service')
 
 const dev = process.env.NODE_ENV !== 'production'
 
+const port = process.env.PORT || 3000
+
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -59,9 +61,9 @@ app
       return handle(req, res)
     })
 
-    server.listen(3000, err => {
+    server.listen(port, err => {
       if (err) throw err
-      console.log('> Ready on http://localhost:3000')
+      console.log('> Ready on http://localhoport')
     })
   })
   .catch(ex => {
